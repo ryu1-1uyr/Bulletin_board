1351cadb7c0a7dff1451d7948bfefd8df2a3edd5
s17001 <s17001@std.iy-college.ac.jp>
2018-10-26 12:41:22 +0900

    edit seen

--- database/seeds/PostCommentSeeder.php
+++ database/seeds/PostCommentSeeder.php
@@ -5,7 +5,7 @@ class PostCommentSeeder extends Seeder{
     public function run(){
         $content = 'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。';
 
-        $commentdammy = 'コメントダミーです。ダミーコメントだよ。';
+        $commentdammy = 'コメントダミーです。ダミーコメント。';
 
         for( $i = 1 ; $i <= 10 ; $i++) {
             $post = new Post;
